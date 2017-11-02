5184751efc774fc448ef445e3c496dc4c6a1fd0a
Oleg Pasko <olegpasko@gmail.com>
2017-11-02 20:15:09 +0200

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'email/index'
 
-  get 'email/save'
-  post 'email/save'
+  resources :emails, only: [:index, :create]
+
+  root 'emails#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
